ffd837ffc9bf975b5e43022125759004901c1d20
aladin <i.aladin@mobidev.biz>
2023-04-10 14:55:41 +0300

    Refactored solution

--- app/javascript/controllers/analytics_controller.js
+++ app/javascript/controllers/analytics_controller.js
@@ -1,51 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
-import * as pbi from 'powerbi-client';
+import * as pbi from "powerbi-client";
 
 export default class extends Controller {
   static targets = ["container"]
-  static values = { url: String }
 
   connect() {
     const reportContainer = this.containerTarget;
     powerbi.bootstrap(reportContainer, { type: "report" });
 
-    fetch(this.urlValue).then(response => response.json()).then(embedConfig => {
+    fetch("/load-embed-config").then(response => response.json()).then(embedConfig => {
       const reportLoadConfig = {
         type: "report",
         tokenType: pbi.models.TokenType.Embed,
         accessToken: embedConfig.token,
-        embedUrl: embedConfig.reports_detail.embed_url,
-        settings: {
-          // background: pbi.models.BackgroundType
-          panes: {
-            bookmarks: {
-              visible: false
-            },
-            fields: {
-              expanded: false
-            },
-            filters: {
-              expanded: false,
-              visible: false
-            },
-            pageNavigation: {
-              visible: false
-            },
-            selection: {
-              visible: true
-            },
-            syncSlicers: {
-              visible: true
-            },
-            visualizations: {
-              expanded: false
-            }
-          }
-        }
+        embedUrl: embedConfig.report_details.embed_url,
       };
       powerbi.embed(reportContainer, reportLoadConfig);
       const iframe = document.getElementsByTagName("iframe")[0];
-      iframe.style.borderRadius = "25px"; 
+      iframe.style.borderRadius = "25px";
     });
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'analytics#index'
+  root 'analytics#index', as: 'index'
 
-  get '/load-report', to: 'analytics#load_report'
+  get '/load-embed-config', to: 'analytics#load_embed_config'
 end
